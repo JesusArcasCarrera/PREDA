559e8f77c7ffc8405405de4e3e0be37b409ba686
Jesus Arcas Carrera <jesusarcascarrera@gmail.com>
2020-12-15 12:03:43 +0100

    start CashRegister.py

---
+++ Algorithms/CashRegister.py
@@ -0,0 +1,15 @@
+""" 
+Cash Register
+Calcular la vuelta
+
+@familia 
+voraz
+dinamica
+
+@descripcion
+Calcula calcula la vuelta despues de pagar en monedas dadas
+Este algoritmo tiene dos partes ya que en ocasiones se calcula mediante voraz y otras mediante dinamica
+
+
+"""
+
