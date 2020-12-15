9a31fe43bf27630ec0ae106f25d3ea238d858556
Jesus Arcas Carrera <jesusarcascarrera@gmail.com>
2020-12-15 12:26:35 +0100

    two voraz implements

--- Algorithms/CashRegister.py
+++ Algorithms/CashRegister.py
@@ -13,3 +13,45 @@ Este algoritmo tiene dos partes ya que en ocasiones se calcula mediante voraz y 
 
 """
 
+from operator import itemgetter
+ 
+
+coins = [1,2,4,8]
+amount = 61
+back = []
+
+coins.sort(reverse=True)
+
+#voraz
+for coin in coins:
+    if amount>=0:
+        back.append(amount//coin)
+        amount %= coin
+        print(back)
+        print(amount)
+
+
+
+
+
+
+# Another voraz implements 
+coins = [('1cent',1),('2cent',2),('4cen',4),('8cent',8)]
+amount = 61
+back = []
+
+coins.sort(key=itemgetter(1),reverse=True)
+
+# voraz
+for text,coin in coins:
+    if amount>=0:
+        ncoins = amount//coin
+        amount %= coin
+
+        back.append(ncoins)
+
+        print('{} coins of {}'.format(ncoins,text))
+        print('Remainig Amount ',amount)
+
+
+        
