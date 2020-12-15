d9da7ea3f84c414a491628f9f2f186bcf990bcb3
Jesus Arcas Carrera <jesusarcascarrera@gmail.com>
2020-12-15 18:25:04 +0100

    fix comments

--- Algorithms/ChangeCalculator.py
+++ Algorithms/ChangeCalculator.py
@@ -7,14 +7,15 @@ voraz
 dinamica
 
 @descripcion
-Calcula calcula la vuelta despues de pagar en monedas dadas
-Este algoritmo tiene dos partes ya que en ocasiones se calcula mediante voraz y otras mediante dinamica
+Calcula la vuelta despues de pagar en monedas dadas
+Este problema tiene varias soluciones ya que se puede calcular mediante voraz y otras mediante dinamica
 
 
 """
 
 from operator import itemgetter
 
+# just print result
 def print_change(change,coins):
     i=0
     for c in change:
@@ -22,13 +23,15 @@ def print_change(change,coins):
             print(f'{c} coins of {coins[i]}')
         i+=1
 
+""" Voraz """
+
 coins = [1,2,4,8]
 amount = 61
 back = []
 
 coins.sort(reverse=True)
 
-#voraz
+
 for coin in coins:
     if amount>=0:
         back.append(amount//coin)
@@ -41,14 +44,15 @@ print_change(back,coins)
 print('-'*50,'\n')
 
 
-# Another voraz implements 
+
+""" Another voraz implements  """
+
 coins = [('1cent',1),('2cent',2),('4cen',4),('8cent',8)]
 amount = 61
 back = []
 
 coins.sort(key=itemgetter(1),reverse=True)
 
-# voraz
 for text,coin in coins:
     if amount>=0:
         ncoins = amount//coin
@@ -63,8 +67,7 @@ for text,coin in coins:
 print('-'*50,'\n')
 
 
-#dynamic
-# non-classical dynamic implementation without matrix
+""" Dynamic; non-classical dynamic implementation without matrix """
 
 coins = [10,6,1]
 amount = 24
@@ -84,22 +87,21 @@ for i in range(len(coins)):
     if sum(loop_back)<sum(best_back) or sum(best_back)==0:
         best_back = loop_back
    
-# just print result
+
 print_change(best_back,coins)
 
 
 
 print('-'*50,'\n')
 
-#recursive
+""" Recursive """
 
 coins = [10,6,1]
-amount = 24
+amount = 38
 best_back = [0]*len(coins) #fill matrix with 0s
 
 
 def change(ind,coins,amount,best_back):
-    print(best_back)
     if ind<len(coins):
         loop_amount = amount
         loop_back = [0]*len(coins)
@@ -117,5 +119,4 @@ def change(ind,coins,amount,best_back):
         return best_back
 
 best_back = change(0,coins, amount,best_back)
-# just print result
 print_change(best_back,coins)
